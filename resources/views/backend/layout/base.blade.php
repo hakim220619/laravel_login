31871f39fe8d046ea8eba5f4ce21018a3fbf67d4
Dani Lukman Hakim <63516526+hakim220619@users.noreply.github.com>
2023-06-26 16:56:53 +0700

    Update base.blade.php

--- resources/views/backend/layout/base.blade.php
+++ resources/views/backend/layout/base.blade.php
@@ -7,7 +7,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Blank page | Lexa - Admin & Dashboard Template</title>
+      <title>{{ Helper::apk()->title }} | {{request()->user()->full_name}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
